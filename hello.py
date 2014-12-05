510e24db3b9b41c428d5ad6adf0fe543efd3f092
Santiago Montoya <santyxdz@gmail.com>
2014-12-05 03:07:42 -0500

    Ejemplo de Variable desde URL

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
     return 'Deployment...'
+
+#Ejemplo de Obtener datos de una Ruta
+@app.route("/w/<name>")
+def helname(name):
+    return "Hello {}!".format(name)
+if __name__ == "__main__":
+    app.run(debug=True)
