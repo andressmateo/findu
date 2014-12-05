29a9d83da99179a1e04aae24d5fb26f66052be23
Santiago Montoya <santyxdz@gmail.com>
2014-12-05 04:44:07 -0500

    Estableciendo Conexion con POSTGRESQL

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from app import views,models
--- config.py
+++ config.py
@@ -3,7 +3,7 @@ __author__ = '?'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = "postgres://pautuijerpsjfe:SGKi84ubN5G8SJjtVbHKV47H2i@ec2-54-243-48-204.compute-1.amazonaws.com:5432/degiqggq043543"
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 #WHOOSH_BASE = os.path.join(basedir, 'search.db')
 
