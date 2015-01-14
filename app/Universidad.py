3ee357971edaedea589597c9bf4d7b679e48997d
andressmateo <andressmateo@andressmateo-HOME.(none)>
2015-01-14 09:31:45 -0500

    Añadido autocompletar con sedes, carreras...

--- app/Universidad.py
+++
@@ -1,7 +0,0 @@
-
-class Universidad:
-
-    def __init__(self, id, nombre, descripcion):
-        self.id = id
-        self.nombre = nombre
-        self.descripcion = descripcion
--- app/search.py
+++ app/search.py
@@ -37,17 +37,3 @@ def search(question, param="all"):
     if param == "all" or param == "u" or param == "o":
         query["o"] = models.OtherName.query.filter(unaccent(func.lower(models.OtherName.name)).contains(question)).all()
     return query
-
-"""
-def levenshtein_distance(str1, str2):
-  d=dict()
-  for i in range(len(str1)+1):
-     d[i]=dict()
-     d[i][0]=i
-  for i in range(len(str2)+1):
-     d[0][i] = i
-  for i in range(1, len(str1)+1):
-     for j in range(1, len(str2)+1):
-        d[i][j] = min(d[i][j-1]+1, d[i-1][j]+1, d[i-1][j-1]+(not str1[i-1] == str2[j-1]))
-  return d[len(str1)][len(str2)]
-"""
