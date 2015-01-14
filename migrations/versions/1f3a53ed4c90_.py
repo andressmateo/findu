6fa81a70162175b7a51c2e0a5b3722a9236c62a5
andressmateo <andressmateo@andressmateo-HOME.(none)>
2015-01-14 17:36:38 -0500

    Arreglando las cosas...

---
+++ migrations/versions/1f3a53ed4c90_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 1f3a53ed4c90
+Revises: 4c3346efafb0
+Create Date: 2015-01-13 16:40:21.167372
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1f3a53ed4c90'
+down_revision = '4c3346efafb0'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/3691f8cc722b_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 3691f8cc722b
+Revises: 26e8f2d25636
+Create Date: 2015-01-13 16:46:15.665116
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3691f8cc722b'
+down_revision = '26e8f2d25636'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
