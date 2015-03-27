bb91f7f7bf023917184fa5449beaa81b2f123ff9
andressmateo <andressmateo@andressmateo-HOME.(none)>
2015-03-27 14:40:53 -0500

    Cambiando la búsqueda

--- app/views.py
+++ app/views.py
@@ -406,7 +406,7 @@ def add_campus():
 
         else:
             campuses = db.session.query(models.UniversityHeadquarter).all()
-            return render_template("form_campus.html",campuses=campuses,  data=select_university_data())
+            return render_template("form_campus.html", campuses=campuses,  data=select_university_data())
 
 
 @app.route("/panel/add_cat_university", methods=['POST', 'GET'])
@@ -415,14 +415,16 @@ def add_cat_university():
         return check_log()
     else:
         if request.args.get("action") == 'add':
-            place = models.UniversityHeadquarter.query.filter_by(id=request.args.get("place_id").encode('utf-8')).first()
-            career = models.Career.query.filter_by(id=request.args.get("career_id").encode('utf-8')).first()
-            cat_university = models.CareerAtUniversity(request.args.get("description").encode('utf-8'), place, career)
-            db.session.add(cat_university)
-            db.session.commit()
-            return str(1)
+            try:
+                place = models.UniversityHeadquarter.query.filter_by(id=request.args.get("place_id")).first()
+                career = models.Career.query.filter_by(id=request.args.get("career_id")).first()
+                cat_university = models.CareerAtUniversity(request.args.get("description").encode('utf-8'), place, career)
+                db.session.add(cat_university)
+                db.session.commit()
+                return str(1)
+            except:
+                return str(0)
         else:
-            #return request.method
             cats_university = db.session.query(models.CareerAtUniversity).all()
             return render_template("form_cat_university.html",cats_university=cats_university,places=select_place_data(),
                                    careers=select_career_data())
