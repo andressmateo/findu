7ac68edb81f537c27bdd4ca0e60c3eff7dea11e5
andressmateo <andressmateo@andressmateo-HOME.(none)>
2015-03-27 15:16:19 -0500

    Cambiando la búsqueda

--- app/views.py
+++ app/views.py
@@ -418,11 +418,11 @@ def add_cat_university():
             try:
                 place = models.UniversityHeadquarter.query.filter_by(id=request.args.get("place_id")).first()
                 career = models.Career.query.filter_by(id=request.args.get("career_id")).first()
-                #cat_university = models.CareerAtUniversity(request.args.get("description").encode('utf-8'), place, career)
-                #db.session.add(cat_university)
-                #db.session.commit()
-                #return str(1)
-                return str(career.name).encode('utf-8')
+                cat_university = models.CareerAtUniversity(request.args.get("description").encode('utf-8'), place, career)
+                db.session.add(cat_university)
+                db.session.commit()
+                return str(1)
+                #return str(career.name).encode('utf-8')
             except Exception, e:
                 return str(e)
         else:
